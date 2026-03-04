61634cbec610b88e7645929964505f70cb60598e
Arknights-Mus <barsuki874@gmail.com>
2026-03-04 12:14:07 +0300

    bot_logic.py

--- bot_logic.py
+++ bot_logic.py
@@ -6,3 +6,9 @@ def gen_pass(pass_length):
     for i in range(pass_length):
         password += random.choice(elements)
     return password
+def flip_coin():
+    flip = random.randint(0, 2)
+    if flip == 0:
+        return "ОРЕЛ"
+    else:
+        return "РЕШКА"
